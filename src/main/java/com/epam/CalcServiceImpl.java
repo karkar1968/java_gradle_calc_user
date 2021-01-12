cb5fd19720122fb0686065b513da5a85a18a0516
Kirill Peshin <kirill_peshin@epam.com>
2021-01-12 13:55:36 +0300

    fail test

--- src/main/java/com/epam/CalcServiceImpl.java
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -18,9 +18,6 @@ public class CalcServiceImpl implements CalcService {
 
     @Override
     public int divide(int a, int b) {
-        if (b == 0) {
-            return 0;
-        }
         return a / b;
     }
 }
