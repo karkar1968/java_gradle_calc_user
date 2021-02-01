f128a883081377de63d85c6e0ae41dc853ee5df1
Kirill Peshin <kirill_peshin@epam.com>
2021-02-01 08:58:48 +0300

    Upd

--- src/main/java/com/epam/CalcServiceImpl.java
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -3,12 +3,14 @@ package com.epam;
 public class CalcServiceImpl implements CalcService {
     @Override
     public int add(int a, int b) {
+        var c = a + b;
         return a + b;
     }
 
     @Override
     public int multiply(int a, int b) {
-        return a * b;
+        int c = a * b;
+        return c;
     }
 
     @Override
