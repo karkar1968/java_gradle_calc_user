7407b6965eb7e32f89c50f51ac17f8d1f7a67621
karkar1968 <123632763+karkar1968@users.noreply.github.com>
2023-08-24 14:03:17 +0600

    Update CalcServiceImpl.java

--- src/main/java/com/epam/CalcServiceImpl.java
+++ src/main/java/com/epam/CalcServiceImpl.java
@@ -23,4 +23,26 @@ public class CalcServiceImpl implements CalcService {
         }
         return a / b;
     }
+     @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int divide(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        return a / b;
+    }
 }
